5742f17f7883db04f1d81d58994115d88cfbb06a
Nolo <26887899+nolosb@users.noreply.github.com>
2022-10-07 18:15:02 +0100

    refactor: use RenderGlimmer

---
+++ javascripts/discourse/widgets/custom-menu.js
@@ -0,0 +1,25 @@
+import { createWidget } from "discourse/widgets/widget";
+import RenderGlimmer from "discourse/widgets/render-glimmer";
+import { hbs } from "ember-cli-htmlbars";
+
+let layouts;
+
+try {
+  layouts = requirejs("discourse/plugins/discourse-layouts/discourse/lib/layouts");
+} catch (error) {
+  layouts = { createLayoutsWidget: createWidget };
+  console.error(error);
+}
+
+export default layouts.createLayoutsWidget(`${settings.widget_name}`, {
+
+  html(attrs, state) {
+    return [
+      new RenderGlimmer(
+        this,
+        "ul.custom-menu",
+        hbs`<CustomMenu/>`
+      ),
+    ];
+  },
+});
