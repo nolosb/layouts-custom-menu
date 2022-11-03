04599413949a78465def73b93b6186b8113b33b8
Nolo <26887899+nolosb@users.noreply.github.com>
2022-11-03 15:32:32 +0000

    feature(custom name): remove custom name and class

--- javascripts/discourse/widgets/custom-menu.js
+++ javascripts/discourse/widgets/custom-menu.js
@@ -11,7 +11,7 @@ try {
   console.error(error);
 }
 
-export default layouts.createLayoutsWidget(`${settings.widget_name}`, {
+export default layouts.createLayoutsWidget("custom-menu", {
 
   html(attrs, state) {
     return [
